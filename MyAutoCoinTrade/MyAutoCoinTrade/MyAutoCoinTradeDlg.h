2e85db42358b6392eb34e73d0aa79ae7ba788b69
Junsu <weekress027@gmail.com>
2022-06-22 16:55:36 +0900

    Make Project

---
+++ MyAutoCoinTrade/MyAutoCoinTrade/MyAutoCoinTradeDlg.h
@@ -0,0 +1,34 @@
+﻿
+// MyAutoCoinTradeDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMyAutoCoinTradeDlg 대화 상자
+class CMyAutoCoinTradeDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMyAutoCoinTradeDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MYAUTOCOINTRADE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
